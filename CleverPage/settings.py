bd44483b5cc16c31564a0d04aace078c58b69cc1
xdanielsb <dniel096@gmail.com>
2016-05-30 17:51:43 -0500

    o

--- CleverPage/settings.py
+++ CleverPage/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOCAL=False
 
