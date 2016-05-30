8b40b0f7f14063a7d2a0c1299d1a00e79f571632
xdanielsb <dniel096@gmail.com>
2016-05-30 13:42:00 -0500

    procfile

--- CleverPage/settings.py
+++ CleverPage/settings.py
@@ -75,19 +75,13 @@ WSGI_APPLICATION = 'CleverPage.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 
-
-DATABASES = {
-'default': dj_database_url.config(default='sqlite:///db.sqlite')
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CleverPage.wsgi --log-file -
