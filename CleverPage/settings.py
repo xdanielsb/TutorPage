169420e3a4fef9011e34fc76db33ffe005f4074c
xdanielsb <dniel096@gmail.com>
2016-05-30 16:52:51 -0500

    change settings

--- CleverPage/settings.py
+++ CleverPage/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -119,16 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+
 
 LOCAL=False
 
 if LOCAL==False:
      STATICFILES_DIRS = (
-          os.path.join(PROJECT_PATH, "static"),
+
      )
  
-     STATIC_ROOT = "/static/"
+     STATIC_ROOT = os.path.join(PROJECT_PATH, "static") 
 else:
      STATIC_ROOT = os.path.join(PROJECT_PATH, "static")
  
