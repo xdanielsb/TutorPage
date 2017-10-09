9424613b3a806894dad62e429f6c1381fba20f26
Daniel Santos <dfsantosbu@unal.edu.co>
2017-10-08 21:47:43 -0500

    Delete virtual env.

--- mypage/logic/content.py
+++
@@ -1,9 +0,0 @@
-def dashBoardContent():
-    dash_content = { "Programs": { "Stochastic Dynamic Programming" :"/dynamicProgramming/",
-                                   "Boltzman Distribution" : "/boltzman/"
-                                 },
-                     "Maraton":  { "Excercice #1":"/1/",
-                                   "Excercice #2":"/2/"             
-                                 } 
-                    }
-    return dash_content
